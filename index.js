6160fa004e009a63f94067eb713597ca377baee2
Kyle Yee <20980584+ShifuYee@users.noreply.github.com>
2018-03-20 16:25:27 -0400

    update index.js to change all 'var's

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var envvar = require('envvar');
-var express = require('express');
-var bodyParser = require('body-parser');
-var moment = require('moment');
-var plaid = require('plaid');
+const envvar = require('envvar');
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const plaid = require('plaid');
 const firebase = require('firebase');
 require('firebase/firestore');
 
 
-var APP_PORT = envvar.number('APP_PORT', 8000);
-var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID');
-var PLAID_SECRET = envvar.string('PLAID_SECRET');
-var PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY');
-var PLAID_ENV = envvar.string('PLAID_ENV', 'development');
+const APP_PORT = envvar.number('APP_PORT', 8000);
+const PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID');
+const PLAID_SECRET = envvar.string('PLAID_SECRET');
+const PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY');
+const PLAID_ENV = envvar.string('PLAID_ENV', 'development');
 
-let REACT_APP_FIREBASE_KEY = "AIzaSyBRLZ8fm4-2rrH-f2kPI2aD7aBg7WzKYhU";
-let REACT_APP_AUTH_DOMAIN = "benevocent.firebaseapp.com";
-let REACT_APP_DATABASE_URL = "https://benevocent.firebaseio.com";
-let REACT_APP_PROJECT_ID = "benevocent";
-let REACT_APP_STORAGE_BUCKET = "benevocent.appspot.com";
-let REACT_APP_MESSAGING_SENDER_ID = "237628622186";
+let REACT_APP_FIREBASE_KEY = 'AIzaSyBRLZ8fm4-2rrH-f2kPI2aD7aBg7WzKYhU';
+let REACT_APP_AUTH_DOMAIN = 'benevocent.firebaseapp.com';
+let REACT_APP_DATABASE_URL = 'https://benevocent.firebaseio.com';
+let REACT_APP_PROJECT_ID = 'benevocent';
+let REACT_APP_STORAGE_BUCKET = 'benevocent.appspot.com';
+let REACT_APP_MESSAGING_SENDER_ID = '237628622186';
 
 
 const config = {
@@ -32,37 +32,25 @@ const config = {
   messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
 };
 
-
-// const config = {
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
-// };
-console.log("config is ", config);
 firebase.initializeApp(config);
 const db = firebase.firestore();
-const firebaseAuth = firebase.auth;
-
 
 
 // We store the access_token in memory - in production, store it in a secure
 // persistent data store
-var ACCESS_TOKEN = null;
-var PUBLIC_TOKEN = null;
-var ITEM_ID = null;
+let ACCESS_TOKEN = null;
+let PUBLIC_TOKEN = null;
+let ITEM_ID = null;
 
 // Initialize the Plaid client
-var client = new plaid.Client(
+const client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
   PLAID_PUBLIC_KEY,
   plaid.environments[PLAID_ENV]
 );
 
-var app = express();
+const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -79,9 +67,9 @@ app.get('/', function(request, response, next) {
 
 app.post('/get_access_token', function(request, response, next) {
   PUBLIC_TOKEN = request.body.public_token;
-  console.log("PUBLIC_TOKEN", PUBLIC_TOKEN);
+  console.log('PUBLIC_TOKEN', PUBLIC_TOKEN);
   client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
-    if (error != null) {
+    if (error !== null) {
       var msg = 'Could not exchange public_token!';
       console.log(msg + '\n' + error);
       return response.json({
@@ -93,7 +81,7 @@ app.post('/get_access_token', function(request, response, next) {
     console.log('Access Token: ' + ACCESS_TOKEN);
     console.log('Item ID: ' + ITEM_ID);
     response.json({
-      'error': false
+      error: false
     });
   });
 });
@@ -102,8 +90,8 @@ app.get('/accounts', function(request, response, next) {
   // Retrieve high-level account information and account and routing numbers
   // for each account associated with the Item.
   client.getAuth(ACCESS_TOKEN, function(error, authResponse) {
-    if (error != null) {
-      var msg = 'Unable to pull accounts from the Plaid API.';
+    if (error !== null) {
+      let msg = 'Unable to pull accounts from the Plaid API.';
       console.log(msg + '\n' + error);
       return response.json({
         error: msg
@@ -123,7 +111,7 @@ app.post('/item', function(request, response, next) {
   // Pull the Item - this includes information about available products,
   // billed products, webhook information, and more.
   client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
-    if (error != null) {
+    if (error !== null) {
       console.log(JSON.stringify(error));
       return response.json({
         error: error
@@ -132,8 +120,8 @@ app.post('/item', function(request, response, next) {
 
     // Also pull information about the institution
     client.getInstitutionById(itemResponse.item.institution_id, function(err, instRes) {
-      if (err != null) {
-        var msg = 'Unable to pull institution information from the Plaid API.';
+      if (err !== null) {
+        let msg = 'Unable to pull institution information from the Plaid API.';
         console.log(msg + '\n' + error);
         return response.json({
           error: msg
@@ -151,13 +139,13 @@ app.post('/item', function(request, response, next) {
 
 app.post('/transactions', function(request, response, next) {
   // Pull transactions for the Item for the last 30 days
-  var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
-  var endDate = moment().format('YYYY-MM-DD');
+  let startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+  let endDate = moment().format('YYYY-MM-DD');
   client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
     count: 250,
     offset: 0,
   }, function(error, transactionsResponse) {
-    if (error != null) {
+    if (error !== null) {
       console.log(JSON.stringify(error));
       return response.json({
         error: error
@@ -173,27 +161,23 @@ app.post('/transactions', function(request, response, next) {
 //response.body
 app.post('/newPurchase', function(request, response, next) {
 
-
   let data = {
     account_id: request.body.account_id,
     amount: request.body.amount,
     date: request.body.date,
     name: request.body.name,
     donation: generateDonation(request.body.amount)
-  }
+  };
 
 
   response.json(data);
 
   return db
     .collection(`all_transactions`)
-    .doc(request.body.userUid)
+    .doc(`${request.body.userUid}`)
     .collection('user_transactions')
     .doc(request.body.transaction_id)
-    .set(data)
-
-  
-
+    .set(data);
 });
 
 function generateDonation(amount){
@@ -221,18 +205,18 @@ function addTransactions(transactions, uid) {
       date: transaction.date,
       name: transaction.name,
       donation: generateDonation(transaction.amount)
-    }
+    };
 
     return db
       .collection(`all_transactions`)
-      .doc(uid)
+      .doc(`${uid}`)
       .collection(`user_transactions`)
       .doc(transaction.transaction_id)
       .set(data);
-  })
+  });
 }
 
 
-var server = app.listen(APP_PORT, function() {
+const server = app.listen(APP_PORT, function() {
   console.log('plaid-walkthrough server listening on port ' + APP_PORT);
 });
